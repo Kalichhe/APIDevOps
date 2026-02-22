ae743c68ff5fdf9ad748e7ae3e9be2a80303542f
Kalichhe <jhondh2003@gmail.com>
2026-02-22 00:19:18 +0000

    :sparkles: Agregando los Schemas y el Modelo de RegistroLabor

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,4 @@
 from app.db.session import Base
 
-# Aquí irás importando tus modelos conforme los crees
-# from app.models.empleado import Empleado
+
+from app.models.registro_labor import RegistroLabor
---
+++ app/models/registro_labor.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.session import Base
+
+class RegistroLabor(Base):
+    __tablename__ = "registro_labores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    empleado_id = Column(Integer, ForeignKey("empleados.id"), nullable=False)
+    codigo_labor = Column(Integer, ForeignKey("labores.codigo_labor"), nullable=False)
+    fecha = Column(Date, nullable=False)
+    cantidad = Column(Float, nullable=False)
+    observacion = Column(String, nullable=True)
+
+    empleado = relationship("Empleado")
+    labor = relationship("Labor")
